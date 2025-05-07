0a4c14bb9d3ce4d95426aeee501f00147a1126d0
Chris Siefert <csiefer2@users.noreply.github.com>
2025-05-07 09:23:01 -0600

    Update build.sh
    Signed-off-by: Chris Siefert <csiefer2@users.noreply.github.com>

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,5 +1,23 @@
-FROM gcr.io/oss-fuzz-base/base-builder:v1       # Base image with clang toolchain
-RUN apt-get update && apt-get install -y build-essential    # Install required packages to build your project.
-COPY . $SRC/<project_name>                      # Copy your project's source code.
-WORKDIR $SRC/<project_name>                     # Working directory for build.sh.
-COPY ./.clusterfuzzlite/build.sh $SRC/           # Copy build.sh into $SRC dir.
+ cmake  \
+            -DTrilinos_ENABLE_ALL_FORWARD_DEP_PACKAGES=OFF \
+            -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES=OFF \
+            -DTrilinos_ENABLE_SECONDARY_TESTED_CODE=OFF \
+            -DTrilinos_ENABLE_Amesos=OFF \
+            -DTrilinos_ENABLE_AztecOO=OFF \
+            -DTrilinos_ENABLE_Epetra=OFF \
+            -DTrilinos_ENABLE_EpetraExt=OFF \
+            -DTrilinos_ENABLE_Ifpack=OFF \
+            -DTrilinos_ENABLE_Intrepid=OFF \
+            -DTrilinos_ENABLE_Isorropia=OFF \
+            -DTrilinos_ENABLE_ML=OFF \
+            -DTrilinos_ENABLE_NewPackage=OFF \
+            -DTrilinos_ENABLE_Pliris=OFF \
+            -DTrilinos_ENABLE_PyTrilinos=OFF \
+            -DTrilinos_ENABLE_ShyLU_DDCore=OFF \
+            -DTrilinos_ENABLE_ThyraEpetraAdapters=OFF \
+            -DTrilinos_ENABLE_ThyraEpetraExtAdapters=OFF \
+            -DLINK_FLAGS="$LIB_FUZZING_ENGINE" \
+            -DTrilinos_ENABLE_Triutils=OFF \
+            ..
+            
+          make -j 16
